c2f3d3216a4cd4bdc3574198fe65278462f7ea14
Shinmera <shinmera@tymoon.eu>
2023-08-04 19:02:14 +0200

    Bleh

--- transforms/raw-ops.lisp
+++ transforms/raw-ops.lisp
@@ -5,6 +5,101 @@
 
 (in-package #:org.shirakumo.fraf.math.transforms)
 
+(define-template t+ <t> (x a b)
+  (let ((type (type-instance 'quat-type <t>)))
+    `((declare (type ,(lisp-type type) x a b)
+               (return-type ,(lisp-type type)))
+      ;; we have to copy aloc here in case (EQ X A)
+      (let ((aloc (vcopy ,(place-form type :location 'a))))
+        (declare (dynamic-extent aloc))
+        (!v* ,(place-form type :location 'x) ,(place-form type :scaling 'a) ,(place-form type :location 'b))
+        (!q* ,(place-form type :location 'x) ,(place-form type :rotation 'a) ,(place-form type :location 'x))
+        (!v+ ,(place-form type :location 'x) ,(place-form type :location 'x) aloc))
+      (!v* ,(place-form type :scaling 'x) ,(place-form type :scaling 'a) ,(place-form type :scaling 'b))
+      (!q* ,(place-form type :rotation 'x) ,(place-form type :rotation 'a) ,(place-form type :rotation 'b))
+      x)))
+
+(define-template t- <t> (x a b)
+  (let ((type (type-instance 'quat-type <t>)))
+    `((declare (type ,(lisp-type type) x a b)
+               (return-type ,(lisp-type type)))
+      
+      x)))
+
+(define-template t= <t> (a b)
+  (let ((type (type-instance 'quat-type <t>)))
+    `((declare (type ,(lisp-type type) a b)
+               (return-type boolean))
+      (and (v= ,(place-form type :location 'a) ,(place-form type :location 'b))
+           (v= ,(place-form type :scaling 'a) ,(place-form type :scaling 'b))
+           (q= ,(place-form type :rotation 'a) ,(place-form type :rotation 'b))))))
+
+(define-template t~= <t> (a b)
+  (let ((type (type-instance 'quat-type <t>)))
+    `((declare (type ,(lisp-type type) a b)
+               (return-type boolean))
+      (and (v~= ,(place-form type :location 'a) ,(place-form type :location 'b))
+           (v~= ,(place-form type :scaling 'a) ,(place-form type :scaling 'b))
+           (q~= ,(place-form type :rotation 'a) ,(place-form type :rotation 'b))))))
+
+(define-template t<- <t> (x a)
+  (let ((type (type-instance 'quat-type <t>)))
+    `((declare (type ,(lisp-type type) x a)
+               (return-type ,(lisp-type type)))
+      (v<- ,(place-form type :location 'x) ,(place-form type :location 'a))
+      (v<- ,(place-form type :scaling 'x) ,(place-form type :scaling 'a))
+      (q<- ,(place-form type :rotation 'x) ,(place-form type :rotation 'a))
+      x)))
+
+(define-template t*v <t> (x a b)
+  (let ((type (type-instance 'quat-type <t>)))
+    `((declare (type ,(lisp-type type) x a)
+               (return-type ,(lisp-type type)))
+      
+      x)))
+
+(define-template t*p <t> (x a b)
+  (let ((type (type-instance 'quat-type <t>)))
+    `((declare (type ,(lisp-type type) x a)
+               (return-type ,(lisp-type type)))
+      
+      x)))
+
+(define-template tinv <t> (x a)
+  (let ((type (type-instance 'quat-type <t>)))
+    `((declare (type ,(lisp-type type) x a)
+               (return-type ,(lisp-type type)))
+      
+      x)))
+
+(define-template t*p-inv <t> (x a b)
+  (let ((type (type-instance 'quat-type <t>)))
+    `((declare (type ,(lisp-type type) x a)
+               (return-type ,(lisp-type type)))
+      
+      x)))
+
+(define-template tmix <t> (x a b tt)
+  (let ((type (type-instance 'quat-type <t>)))
+    `((declare (type ,(lisp-type type) x a b)
+               (return-type ,(lisp-type type)))
+      
+      x)))
+
+(define-template tmat <s> <t> (x a)
+  (let ((type (type-instance 'quat-type <t>)))
+    `((declare (type ,(lisp-type type) a)
+               (return-type ,(lisp-type type)))
+      
+      x)))
+
+(define-template tfrom-mat <s> <t> (x a)
+  (let ((type (type-instance 'quat-type <t>)))
+    `((declare (type ,(lisp-type type) x)
+               (return-type ,(lisp-type type)))
+      
+      x)))
+
 ;; [ ] t+
 ;; [ ] t-
 ;; [ ] t=
